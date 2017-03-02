58ec8cf03ba82f3ddc179ca0df7b664fa97f06d3
curso18 <curso18java@gmail.com>
2017-03-02 13:48:41 +0100

    Antes de verificar con cliente: clase averia montada

---
+++ src/main/java/es/cic/curso/curso18/ejercicio028/backend/dominio/Averia.java
@@ -0,0 +1,52 @@
+package es.cic.curso.curso18.ejercicio028.backend.dominio;
+
+import es.cic.curso.curso18.ejercicio028.backend.repository.Identificable;
+
+public class Averia implements Identificable<Long> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4583881797217483524L;
+	
+	long Pk_idAveria;
+	String nombre;
+	String descripcion;
+
+	public Averia() {
+		super();
+	}
+
+	public Averia(String nombre, String descripcion) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	@Override
+	public Long getId() {
+		return Pk_idAveria;
+	}
+
+	@Override
+	public void setId(Long Pk_idAveria) {
+		this.Pk_idAveria=Pk_idAveria;
+	}
+
+}
