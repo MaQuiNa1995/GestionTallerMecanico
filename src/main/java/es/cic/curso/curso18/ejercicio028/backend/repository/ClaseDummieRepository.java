94891e40769ea1ddb7b441c1a0a8e7242e78700e
curso18 <curso18java@gmail.com>
2017-03-02 18:06:51 +0100

    Eliminacion de la ClaseDummie y todo lo relacionado con ella

--- src/main/java/es/cic/curso/curso18/ejercicio028/backend/repository/ClaseDummieRepository.java
+++
@@ -1,7 +0,0 @@
-package es.cic.curso.curso18.ejercicio028.backend.repository;
-
-import es.cic.curso.curso18.ejercicio028.backend.dominio.Dummie;
-
-public interface ClaseDummieRepository extends IRepository<Long, Dummie> {
-
-}
