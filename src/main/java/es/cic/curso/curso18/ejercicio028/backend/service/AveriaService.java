dcc73d4fe66a8314fcde2fe0ba22b2a13a3572db
curso18 <curso18java@gmail.com>
2017-03-02 14:58:41 +0100

    ultimos cambios en clase antes de comer

--- src/main/java/es/cic/curso/curso18/ejercicio028/backend/service/AveriaService.java
+++ src/main/java/es/cic/curso/curso18/ejercicio028/backend/service/AveriaService.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package es.cic.curso.curso18.ejercicio028.backend.service;
 
 import java.util.List;
 
 import es.cic.curso.curso18.ejercicio028.backend.dominio.Averia;
 
-/**
- *
- * @author MaQuiNa
- */
 public interface AveriaService {
 
     Averia actualizarAveria(Averia modificada);
