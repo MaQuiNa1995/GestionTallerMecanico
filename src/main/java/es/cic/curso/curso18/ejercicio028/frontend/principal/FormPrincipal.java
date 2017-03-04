dae2ea4363c2004a329462e49f5e0379c8ef5f89
curso18 <curso18java@gmail.com>
2017-03-04 14:43:13 +0100

    ventanas sin Grid hechas

--- src/main/java/es/cic/curso/curso18/ejercicio028/frontend/principal/FormPrincipal.java
+++ src/main/java/es/cic/curso/curso18/ejercicio028/frontend/principal/FormPrincipal.java
@@ -7,8 +7,12 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+import es.cic.curso.curso18.ejercicio028.frontend.secundarias.GestionAverias;
+import es.cic.curso.curso18.ejercicio028.frontend.secundarias.GestionTiposVehiculos;
 
 @Theme("myTheme")
 public class FormPrincipal extends UI {
@@ -18,36 +22,34 @@ public class FormPrincipal extends UI {
 	 */
 	private static final long serialVersionUID = -1598331557630407298L;
 
+	HorizontalLayout panelTodo=null;
+	TabSheet menu=null;
+	
 	@Override
 	protected void init(VaadinRequest request) {
-		
-		final HorizontalLayout panelTodo = new HorizontalLayout();
+		definirPanelTodo();
+		definirMenuPestanas();
+		setContent(panelTodo);
+	}
+	
+	private void definirPanelTodo(){
+		panelTodo = new HorizontalLayout();
 		panelTodo.setMargin(true);
 		panelTodo.setSpacing(true);
 		panelTodo.setWidth("100%");
-		
-		Label labelDummie = new Label("Prueba Dummie De La Puesta a Punto De Vaadin");
-		
-		/**
-		 * final VerticalLayout gridFiguras = new GridFiguras();
-		 
-		
-		final VerticalLayout annadirFiguras = new AnnadirFiguras();
-		final VerticalLayout eliminarFiguras = new EliminarFiguras();
-		final VerticalLayout actualizarFiguras = new ActualizarFiguras();
-		final VerticalLayout listarFiguras = new ListarFiguras();
-
-		TabSheet menu = new TabSheet();
-        menu.setHeight(100.0f, Unit.PERCENTAGE);
-        menu.addTab(, "Añadir Figuras");
-        menu.addTab(, "Eliminar Figuras");
-        menu.addTab(, "Actualizar Figuras");
-        menu.addTab(, "listar Figuras");
-        */
+	}
 	
-		panelTodo.addComponent(labelDummie);
-		
-		setContent(panelTodo);
+	private void definirMenuPestanas(){
+		menu = new TabSheet();
+        menu.setHeight(100.0f, Unit.PERCENTAGE);
+        
+		VerticalLayout gestionTiposVehiculos = new GestionTiposVehiculos();
+		VerticalLayout gestionAverias = new GestionAverias();
+        
+        menu.addTab(gestionTiposVehiculos, "Gestión Tipos De Vehiculos");
+        menu.addTab(gestionAverias, "Gestión De Averías");
+        
+        panelTodo.addComponent(menu);
 	}
 	
 	@WebServlet(value = "/*", asyncSupported = true)
