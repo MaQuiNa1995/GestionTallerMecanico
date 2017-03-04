0c12dd4407bcfd48a48e9136d90665c910de8d7b
curso18 <curso18java@gmail.com>
2017-03-04 14:43:30 +0100

    ventanas sin Grid hechas 2 (faltaba git add)

---
+++ src/main/java/es/cic/curso/curso18/ejercicio028/frontend/secundarias/GestionAverias.java
@@ -0,0 +1,77 @@
+package es.cic.curso.curso18.ejercicio028.frontend.secundarias;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+
+public class GestionAverias extends VerticalLayout {
+
+	VerticalLayout panelTodo;
+	VerticalLayout panelDatos;
+	HorizontalLayout panelBotones;
+	
+	public GestionAverias() {
+		super();
+		definirPanelTodo();
+		definirPanelDatos();
+		definirPanelBotones();
+		agregarAPanelTodo();
+	}
+	
+	private void definirPanelBotones(){
+		
+		panelBotones = new HorizontalLayout();
+		
+		Button anadir,eliminar; 
+		
+		anadir = new Button("Dar De Alta");
+		eliminar = new Button("Dar De Baja");
+		
+		panelBotones.addComponents(anadir,eliminar);
+	}
+	
+	private void definirPanelDatos(){
+		
+		panelDatos = new VerticalLayout();
+		
+		HorizontalLayout panelIntroducirDatos = new HorizontalLayout();
+		HorizontalLayout panelConfirmacion = new HorizontalLayout();
+		
+		TextField nombre,
+				descripcion;
+		
+		Button confirmar,
+				cancelar;
+		
+		ComboBox vehiculos;
+		
+		nombre = new TextField("Introduce El Nombre De la Avería:");
+		descripcion = new TextField("Introduce La Descripción:");
+		vehiculos= new ComboBox();
+		
+		panelIntroducirDatos.addComponents(nombre,descripcion,vehiculos);
+		
+		confirmar = new Button("Confirmar");
+		cancelar = new Button("Cancelar");
+		
+		panelConfirmacion.addComponents(confirmar,cancelar);
+		
+		panelDatos.addComponents(panelIntroducirDatos,panelConfirmacion);
+		
+	}
+	
+	private void definirPanelTodo(){
+		panelTodo = new VerticalLayout();
+		panelTodo.setMargin(true);
+		panelTodo.setSpacing(true);
+		panelTodo.setWidth("100%");
+	}
+	
+	private void agregarAPanelTodo(){
+		panelTodo.addComponents(panelBotones,panelDatos);
+	}
+	
+	
+}
---
+++ src/main/java/es/cic/curso/curso18/ejercicio028/frontend/secundarias/GestionTiposVehiculos.java
@@ -0,0 +1,99 @@
+package es.cic.curso.curso18.ejercicio028.frontend.secundarias;
+
+import java.util.List;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+
+import es.cic.curso.curso18.ejercicio028.backend.dominio.Vehiculo;
+import es.cic.curso.curso18.ejercicio028.backend.service.VehiculoServiceImpl;
+
+public class GestionTiposVehiculos extends VerticalLayout {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2370949761559912513L;
+	
+	VerticalLayout panelTodo;
+	VerticalLayout panelDatos;
+	HorizontalLayout panelBotones;
+	
+	ComboBox comboVehiculos;
+	
+	public GestionTiposVehiculos() {
+		super();
+		definirPanelTodo();
+		definirPanelDatos();
+		definirPanelBotones();
+		agregarAPanelTodo();
+	}
+	
+	private void definirPanelBotones(){
+		
+		panelBotones = new HorizontalLayout();
+		
+		Button anadir,eliminar; 
+		
+		anadir = new Button("Dar De Alta");
+		eliminar = new Button("Dar De Baja");
+		
+		panelBotones.addComponents(anadir,eliminar);
+	}
+	
+	private void definirPanelDatos(){
+		
+		panelDatos = new VerticalLayout();
+		
+		HorizontalLayout panelIntroducirDatos = new HorizontalLayout();
+		HorizontalLayout panelConfirmacion = new HorizontalLayout();
+		
+		TextField tipo,
+				descripcion;
+		
+		Button confirmar,
+				cancelar;
+		
+		tipo = new TextField("Introduce Nombre Del Tipo De Vehiculo:");
+		descripcion = new TextField("Introduce La Descripción:");
+		comboVehiculos= new ComboBox();
+		
+		refrescarComboBox();
+		
+		panelIntroducirDatos.addComponents(tipo,descripcion,comboVehiculos);
+		
+		confirmar = new Button("Confirmar");
+		cancelar = new Button("Cancelar");
+		
+		panelConfirmacion.addComponents(confirmar,cancelar);
+		
+		panelDatos.addComponents(panelIntroducirDatos,panelConfirmacion);
+		
+	}
+	
+	private void definirPanelTodo(){
+		panelTodo = new VerticalLayout();
+		panelTodo.setMargin(true);
+		panelTodo.setSpacing(true);
+		panelTodo.setWidth("100%");
+	}
+	
+	private void agregarAPanelTodo(){
+		panelTodo.addComponents(panelBotones,panelDatos);
+	}
+	
+	private void refrescarComboBox(){
+		VehiculoServiceImpl vehiculoService = new VehiculoServiceImpl();
+		
+		List<Vehiculo> listaVehiculos = vehiculoService.obtenerVehiculos();
+		
+		for (Vehiculo vehiculo : listaVehiculos) {
+			comboVehiculos.addItem(vehiculo.getNombre());
+		}
+	}
+	
+	
+}
