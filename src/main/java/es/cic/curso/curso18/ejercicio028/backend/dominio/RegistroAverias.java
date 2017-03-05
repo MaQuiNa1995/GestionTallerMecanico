785240306a161976437c9551c90270f5c92dcd34
curso18 <curso18java@gmail.com>
2017-03-05 18:42:41 +0100

    Nuevas clases creadas

---
+++ src/main/java/es/cic/curso/curso18/ejercicio028/backend/dominio/RegistroAverias.java
@@ -0,0 +1,74 @@
+package es.cic.curso.curso18.ejercicio028.backend.dominio;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import es.cic.curso.curso18.ejercicio028.backend.repository.Identificable;
+
+@Entity
+@Table(name = "REGISTROAVERIAS")
+public class RegistroAverias implements Identificable<Long>{
+
+		/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1478940692428183893L;
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		Long pkidRegistroAverias;
+
+		@Column(name = "fecha")
+		String fecha;
+		
+		@JoinColumn(name = "fkidAveria")
+		@ManyToOne(fetch = FetchType.LAZY)
+		Averia averia;
+
+		public RegistroAverias() {
+			super();
+		}
+
+		public RegistroAverias(String fecha, Averia averia) {
+			super();
+			this.fecha = fecha;
+			this.averia = averia;
+		}
+
+		public String getFecha() {
+			return fecha;
+		}
+
+		public void setFecha(String fecha) {
+			this.fecha = fecha;
+		}
+
+		public Averia getAveria() {
+			return averia;
+		}
+
+		public void setAveria(Averia averia) {
+			this.averia = averia;
+		}
+
+		@Override
+		public Long getId() {
+			return pkidRegistroAverias;
+		}
+
+		public void setId(Long pkidRegistroAverias) {
+			this.pkidRegistroAverias = pkidRegistroAverias;
+		}
+		
+		
+
+	}
+	
+
---
+++ src/main/java/es/cic/curso/curso18/ejercicio028/backend/dominio/VehiculosTienenAverias.java
@@ -0,0 +1,61 @@
+package es.cic.curso.curso18.ejercicio028.backend.dominio;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import es.cic.curso.curso18.ejercicio028.backend.repository.Identificable;
+
+@Entity
+@Table(name = "VEHICULOSTIENENAVERIAS")
+public class VehiculosTienenAverias implements Identificable<Long> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4297447102360376912L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long pkidVehiculosTienenAverias;
+
+	@JoinColumn(name = "fkidVehiculosAverias")
+	@ManyToOne(fetch = FetchType.LAZY)
+	RegistroAverias registroAverias;
+
+	@JoinColumn(name = "fkidAveriasVehiculos")
+	@ManyToOne(fetch = FetchType.LAZY)
+	Vehiculo vehiculo;
+
+	public RegistroAverias getRegistroAverias() {
+		return registroAverias;
+	}
+
+	public void setRegistroAverias(RegistroAverias registroAverias) {
+		this.registroAverias = registroAverias;
+	}
+
+	public Vehiculo getVehiculo() {
+		return vehiculo;
+	}
+
+	public void setVehiculo(Vehiculo vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+
+	@Override
+	public Long getId() {
+		return pkidVehiculosTienenAverias;
+	}
+
+	@Override
+	public void setId(Long pkidVehiculosTienenAverias) {
+		this.pkidVehiculosTienenAverias = pkidVehiculosTienenAverias;
+	}
+
+}
